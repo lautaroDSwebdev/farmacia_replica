39c5fbc3556e5072dd29a8bd9584ba199fab5e89
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2025-02-03 07:01:36 -0300

    fix: no used values

--- src/components/pages/homepage/SectionsHome/bannerHeader/CarruselComponent.tsx
+++ src/components/pages/homepage/SectionsHome/bannerHeader/CarruselComponent.tsx
@@ -2,7 +2,7 @@
 import data from '../../../../../mock/data.json'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import {  useState } from "react";
+// import {  useState } from "react";
 import { Link } from "react-router-dom";
 import "./style.css"
 const { BannerImgDesk , BannerImgMob}  = data.entireHome
@@ -11,21 +11,22 @@ const { BannerImgDesk , BannerImgMob}  = data.entireHome
 const CarruselComponent = () => {
     let value
     clearInterval(value)
-    const [count, setCount] = useState(0)
 
-    const Autoplay = () => { 
-        // si el contador es menor a el ultimo indice del array
-        if(count < BannerImgDesk.length -1){
-          setCount(count +1)
-          }else{
-            setCount(0)
-          }
-        if(count < BannerImgMob.length -1){
-          setCount(count +1)
-          }else{
-            setCount(0)
-          }
-        }
+    // const [count, setCount] = useState(0)
+
+    // const Autoplay = () => { 
+    //     // si el contador es menor a el ultimo indice del array
+    //     if(count < BannerImgDesk.length -1){
+    //       setCount(count +1)
+    //       }else{
+    //         setCount(0)
+    //       }
+    //     if(count < BannerImgMob.length -1){
+    //       setCount(count +1)
+    //       }else{
+    //         setCount(0)
+    //       }
+    //     }
     
         // useEffect(()=> {
         //     value = setTimeout(Autoplay, 5000)
@@ -34,9 +35,9 @@ const CarruselComponent = () => {
    
   return (
     <div>
-            <Link  to={BannerImgDesk[count].link}>
-                <div className='image-bg hidden md:block'  key={BannerImgDesk[count].id} style={{backgroundImage: `url(${BannerImgDesk[count].img})`}}>  </div>
-                <div className='image-mob block md:hidden'  key={BannerImgMob[count].id} style={{backgroundImage: `url(${BannerImgMob[count].imgMobile})`}}>  </div>
+            <Link  to={BannerImgDesk[0].link}>
+                <div className='image-bg hidden md:block'  key={BannerImgDesk[0].id} style={{backgroundImage: `url(${BannerImgDesk[0].img})`}}>  </div>
+                <div className='image-mob block md:hidden'  key={BannerImgMob[0].id} style={{backgroundImage: `url(${BannerImgMob[0].imgMobile})`}}>  </div>
             </Link>
         </div>
   )
